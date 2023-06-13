0b4332cf4eefea45d574c647cf593c10ccbdf808
Mark Nguy <56264886+Qischer@users.noreply.github.com>
2023-06-13 11:02:04 -0400

    fixed import

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import './App.css';
 
 import Navbar from './components/Navbar';
 import Intro from './components/Intro';
-import Projects from './components/Projects';
-import About from './components/About';
 import Content from './components/Content';
 
 function App() {
