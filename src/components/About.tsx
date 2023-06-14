3b7d273985561824520a73120140ca6c1b764f53
Mark Nguy <56264886+Qischer@users.noreply.github.com>
2023-06-14 10:22:15 -0400

    commit

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,7 +5,7 @@ function About() {
     <Parallax speed={-10} className="h-screen">
       <div className=" static rounded-md w-4/5 m-auto mt-48 md:h-5/6 h-3/5 bg-[url('/Profile.png')] bg-cover bg-center">
         <Parallax speed={-10}>
-          <div className="md:flex hidden sm:absolute left-3/4 rounded-md w-2/5 h-96 bg-red-50"></div>
+          <div className="md:flex hidden md:absolute left-3/4 rounded-md w-2/5 h-96 bg-red-50"></div>
         </Parallax>
       </div>
       <div className="md:hidden mt-3 rounded-md w-4/5 m-auto h-1/5 bg-red-100 "></div>
