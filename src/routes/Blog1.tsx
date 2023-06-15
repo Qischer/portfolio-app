5e8329fe78a7605323946a7d087f51d77076b568
Mark Nguy <56264886+Qischer@users.noreply.github.com>
2023-06-15 11:47:51 -0400

    Update Blog1.tsx

--- src/routes/Blog1.tsx
+++ src/routes/Blog1.tsx
@@ -2,32 +2,11 @@ import NavbarBlog from '../components/Blog/NavbarBlog';
 
 export default function Blog1() {
   return (
-    <>
-      <div className=" max-w-5xl px-3 m-auto">
-        <div className=" rounded-md bg-slate-500 m-auto mt-40 h-full"></div>
-        <div className=" text-left">
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-            fermentum, dui id commodo elementum, risus mauris cursus libero, a
-            consectetur neque quam sed ligula. Aliquam bibendum in justo vitae
-            scelerisque. Curabitur commodo mauris dolor, et vestibulum velit
-            blandit quis. Nunc gravida egestas consequat. Donec id diam vitae
-            lacus pellentesque sagittis. Maecenas eu ante nulla. Proin ut massa
-            ut nulla ultrices aliquet. Phasellus sollicitudin ex et purus
-            elementum, nec sollicitudin odio vehicula. Donec aliquam euismod
-            quam.
-          </p>
-          <p>
-            Praesent ex elit, aliquet vehicula feugiat ullamcorper, imperdiet
-            nec urna. Maecenas ac dui leo. Aenean a augue tellus. Sed blandit
-            magna quis felis vehicula, sit amet mollis elit rhoncus. Nam viverra
-            lectus lectus, in fringilla nibh iaculis ut. Quisque semper, arcu
-            nec hendrerit tincidunt, justo justo consectetur ligula, sed
-            tincidunt augue eros vel quam. Nulla pulvinar sed elit sit amet
-            condimentum. Praesent ut pulvinar ante.
-          </p>
-        </div>
+    <main className="bg-orange-50">
+      <NavbarBlog></NavbarBlog>
+      <div className="p-5">
+        <div className=" rounded-md h-96 bg-rose-400"></div>
       </div>
-    </>
+    </main>
   );
 }
